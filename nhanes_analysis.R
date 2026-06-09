ca9cb61d55935b550657ec439e361af5135369ee
Tyler Connolly <tylerpconnolly@gmail.com>
2026-06-09 15:35:18 -0700

    Trimmed demographic data set to include only variables necessary for analysis.

--- nhanes_analysis.R
+++ nhanes_analysis.R
@@ -1,13 +1,33 @@
-# Loading libraries
+# 01 Load Packages ----
 library(nhanesA)
 library(tidyverse)
+library(gtsummary)
+library(survey)
 
-# Pull demo data for 2017-2018 cycle
+# 02 Import Data ----
 demo <- nhanes('DEMO_J')
 
-# Quick look
-glimpse(demo)
-names(demo)
+# 03 Clean and Recode ----
+# Create vector of variable names for trimmed dataset
+selection <- c(
+  veteran = "DMQMILIZ",
+  age_years = "RIDAGEYR",
+  gender = "RIAGENDR",
+  ethnicity = "RIDRETH3",
+  hh_income = "INDHHIN2",
+  education = "DMDEDUC2"
+)
+# Pass in variable vector to trim data set, change yes/no to veteran/non-veteran
+# filter out missing data. 
+demo_clean <- demo %>%
+  select(all_of(selection)) %>%
+  mutate(
+    veteran = case_when(
+      veteran == "Yes" ~ "Veteran",
+      veteran == "No" ~ "Non-Veteran",
+      TRUE ~ NA_character_
+    )
+  ) %>%
+  filter(!is.na(veteran))
 
-# Checking military service var
-table(demo$DMQMILIZ, useNA = "always")
+
